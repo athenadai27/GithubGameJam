dd91037cd80520f790a7612a3494c87f6313a40d
Xiangyu Wan <johnnywon@g.ucla.edu>
2021-11-12 12:38:27 -0800

    some cleanup
    turns out we don't need to modify yarn spinner to let it spit out a line at a time
    TextController will be modified to support floating word bubbles

---
+++ Github Game Jam/Assets/Script/DialogSystem/TextController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Yarn.Unity;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject wordContainerPrefab;
+
+    [SerializeField]
+    DialogueRunner runner;
+
+    public bool inDialog { get; set; } = false;
+    Dictionary<string, Sprite> nameToPfp = new Dictionary<string, Sprite>();
+
+    public static TextController instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowLine(string line)
+    {
+        // TODO: break line
+        string[] words = line.Split(' ');
+        
+        // TODO: put each word into a block
+
+        // TODO: float up blocks by some sequence (first iteration just send them up together)
+    }
+
+
+    public void StartDialog(string node)
+    {
+        if (!inDialog)
+            runner.StartDialogue(node);
+    }
+}
