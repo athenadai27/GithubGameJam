7304e41b30ee149058ac1c92fa0eac9698becebd
Xiangyu Wan <johnnywon@g.ucla.edu>
2021-11-28 12:21:09 -0800

    kind of implemented volume checkpoint

---
+++ Github Game Jam/Assets/Script/CheckPoint System/CheckPoint.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+/// <summary>
+/// Parent class of check point types
+/// </summary>
+public class CheckPoint : MonoBehaviour
+{
+    static protected CheckPoint activeCheckPoint = null;
+
+    [SerializeField]
+    protected UnityEvent beforeTriggering;
+    [SerializeField]
+    protected UnityEvent afterTriggering;
+
+    [SerializeField]
+    protected UnityEvent onLoad;
+
+    PlayerController player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+
+    private Vector3 savedPosition;
+
+    static public void LoadCheckPoint()
+    {
+        if (activeCheckPoint)
+            activeCheckPoint.Load();
+    }
+
+    private void Load()
+    {
+        // restore player controller
+        player.Reset();
+
+        // seems to reset the flower?
+        ItemScript[] items = FindObjectsOfType<ItemScript>();
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i].Break();
+        }
+
+        // reset position
+        player.transform.position = savedPosition;
+
+        onLoad.Invoke();
+    }
+
+    protected void Save()
+    {
+        beforeTriggering.Invoke();
+
+        savedPosition = player.transform.position;
+        activeCheckPoint = this;
+
+        afterTriggering.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        activeCheckPoint = null;
+    }
+}
---
+++ Github Game Jam/Assets/Script/CheckPoint System/VolumnCheckPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VolumnCheckPoint : CheckPoint
+{ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.collider.gameObject.tag.Contains("Player"))
+            return;
+
+        Save();
+    }
+}
