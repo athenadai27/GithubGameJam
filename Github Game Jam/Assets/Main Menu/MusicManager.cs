015971a8154da22a08ba160fa98880a6e1ccce2c
Xiangyu Wan <johnnywon@g.ucla.edu>
2021-11-30 23:29:41 -0800

    implemented music manager

--- Github Game Jam/Assets/Main Menu/MusicManager.cs
+++ Github Game Jam/Assets/Main Menu/MusicManager.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class MusicManager : MonoBehaviour
 {
     public static MusicManager mPlayer;
     
+    [Serializable]
+    public struct SceneClipPair
+    {
+        public string sceneName;
+        public AudioClip clip;
+    }
+
+    [SerializeField]
+    List<SceneClipPair> clipMappings;
+
+    AudioSource audioSource;
+
     void Awake()
     {
         if (mPlayer != null)
@@ -18,11 +33,34 @@ public class MusicManager : MonoBehaviour
             mPlayer = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
-    void Start()
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        //this.gameObject.GetComponent<AudioSource>().Play();
+        AudioClip newClip = null;
+
+        foreach (var pair in clipMappings)
+        {
+            if (pair.sceneName == scene.name)
+            {
+                newClip = pair.clip;
+                break;
+            }
+        }
+
+        if (newClip)
+        {
+            audioSource.clip = newClip;
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
 
 }
