bddd84fae76d6f7b522e30dd8b55244a67819662
Xiangyu Wan <johnnywon@g.ucla.edu>
2021-12-01 01:07:31 -0800

    removed added code

--- Github Game Jam/Assets/FrogKingScript.cs
+++ Github Game Jam/Assets/FrogKingScript.cs
@@ -861,33 +861,4 @@ public class FrogKingScript : MonoBehaviour
         tongueScript.tongue.gameObject.SetActive(false);
         resetting = false;
     }
-
-    public void Reset(int inPhase)
-    {
-        if (activeTextController != null)
-        {
-            activeTextController.SetActive(false);
-            activeTextController = null;
-        }
-        if (textControllerScript != null)
-        {
-            textControllerScript.FadeText();
-            textControllerScript = null;
-        }
-        for (int i = 0; i < oozeHolder.transform.childCount; i++)
-        {
-            oozeHolder.transform.GetChild(i).gameObject.SetActive(false);
-        }
-        for (int i = 0; i < sonicWavesHolder.transform.childCount; i++)
-        {
-            sonicWavesHolder.transform.GetChild(i).gameObject.SetActive(false);
-        }
-
-        cameraShaking = false;
-        currentPhase = (BossStates) inPhase;
-        bossState = (BossStates) inPhase;
-        myAnim.Rebind();
-        tongueScript.tongue.gameObject.SetActive(false);
-        resetting = false;
-    }
 }
